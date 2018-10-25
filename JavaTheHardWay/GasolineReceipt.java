e78abe2b97b839ca53b2d8a8365b95827275901f
Patrick <patrick.schwindt14@gmail.com>
2018-10-24 23:24:25 -0600

    Finish Java The Hard Way Exercise 1

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,14 @@
+public class GasolineReceipt{
+  public static void main ( String[] args ){
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      CORNER STORE      |" );
+    System.out.println( "| 2015-03-29  04:38PM    |" );
+    System.out.println( "| Gallons:       10.87   |");
+    System.out.println( "| Price/gallon: $ 2.089  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel Total:  $ 22.71   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
