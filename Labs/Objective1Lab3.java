9c880f0d703f7c14f81e0dcc125e874723d67f5d
Patrick <patrick.schwindt14@gmail.com>
2018-10-25 00:00:15 -0600

    Complete Objective 1 Lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.println("Name : Patrick Schwindt");
+    System.out.println("Age : 28");
+    System.out.println("State : Colorado");
+  }
+}
