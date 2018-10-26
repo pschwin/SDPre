31e0780fea37f6f2ff9749f4994e07e4a999e7aa
Patrick <patrick.schwindt14@gmail.com>
2018-10-25 20:03:24 -0600

    Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2{
+  public static void main (String[] args){
+    String name = "Patrick Schwindt";
+    int age = 28;
+    String state = "Colorado";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+
+  }
+}
