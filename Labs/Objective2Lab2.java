62a5067c290b7bfec26fad5f0f8bc1b9d08ac974
Patrick <patrick.schwindt14@gmail.com>
2018-10-25 20:09:38 -0600

    Objective 2 Lab 2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -4,9 +4,7 @@ public class Objective2Lab2{
     int age = 28;
     String state = "Colorado";
 
-    System.out.println(name);
-    System.out.println(age);
-    System.out.println(state);
+    System.out.println(name + " " + age + " " + state);
 
   }
 }
