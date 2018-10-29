6b9c23934711f76e3b2e3cb3de55e02127e7ab4f
Patrick <patrick.schwindt14@gmail.com>
2018-10-28 21:19:00 -0600

    Objective 6 Lab 5

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+public class Objective6Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+    String food1 = "Apple";
+    String food2 = "Banana";
+    String food3 = "Coconut";
+
+    while (true) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if (selection == 1){
+        System.out.println("Hello World");
+      }
+
+      else if (selection == 2){
+        System.out.println(food1 + ", " + food2 + ", " + food3 );
+      }
+
+      else if  (selection == 3){
+        System.out.println("Goodbye");
+        break;
+
+      }
+
+    }
+    scanner.close();
+  }
+
+}
