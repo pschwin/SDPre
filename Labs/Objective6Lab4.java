afaaf46f54cfe4a88a9be49c7b0ed5d767e7999a
Patrick <patrick.schwindt14@gmail.com>
2018-10-28 18:16:14 -0600

    Objective 6 Lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4{
+  public static void main(String[] args){
+    int counter = 0;
+    int sum = 0;
+
+    while (counter != 20){
+      counter = (counter + 1);
+      sum += counter;
+    }
+
+    System.out.println(sum);
+
+  }
+}
